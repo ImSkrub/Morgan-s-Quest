1971f630600c6e310e4f15e2caf991c66109102e
ficticious <magustinmeli@gmail.com>
2024-12-18 13:58:47 -0300

    contador

--- Morgan's Quest/Assets/Scripts/Enemy/ChildLife.cs
+++ Morgan's Quest/Assets/Scripts/Enemy/ChildLife.cs
@@ -128,6 +128,9 @@ public class ChildLife : MonoBehaviour
         enemyTree.EliminarElem(gameObject.name);
         OnDeath?.Invoke();
 
+        // Disminuir el contador de enemigos en el GameManager
+        GameManager.Instance.DecreaseCounter();
+
         Collider2D collider = GetComponent<Collider2D>();
         if (collider != null)
         {
